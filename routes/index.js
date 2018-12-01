b73df7bbdf42a7cb8f9cbba9f85bba1a017640de
hyunkwanko <cjdshrwkd11@gmail.com>
2018-12-01 20:53:26 +0900

    Everyolo

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/login', (request, response) => {
 router.post('/login_process', (request, response) => {
     var post = request.body;
     db.query(`SELECT * FROM user WHERE username=? AND password=?`, [post.username, post.password], (err, topic) => {
-        if (topic[0].username != undefined){
+        if (topic[0].username != undefined){ // Registered
             response.redirect(`/?id=${post.username}`);
         }
     });
@@ -34,18 +34,31 @@ router.get('/signup', (request, response) => {
 
 router.post('/signup_process', (request, response) => {
     var post = request.body;
-    db.query(`INSERT INTO user (username, password) VALUES(?, ?)`, [post.username, post.password], (err, topic) => {
-        db.connect((err) => {
-            var sql = `CREATE TABLE ${post.username}(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                title VARCHAR(40),
-                description TEXT,
-                source VARCHAR(40)
-            )`;
-            db.query(sql, (err, result) => {}); // 이미 존재해도 상관없다.
-            console.log(post.username + "Table Created!");
-        });        
-        response.redirect(`/login`);
+    db.query(`SELECT * FROM user WHERE username=?`, [post.username], (err, topic) => {
+        if (!err) { // Already Exist
+            console.log(err);
+            response.send(`
+                <script type="text/javascript">
+                    alert("이미 존재하는 아이디");	
+                    window.location.href = '/signup';
+                </script>
+            `);
+            return;
+        } else {
+            db.query(`INSERT INTO user (username, password) VALUES(?, ?)`, [post.username, post.password], (err, topic) => {
+                db.connect((err) => {
+                    var sql = `CREATE TABLE ${post.username}(
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        title VARCHAR(40),
+                        description TEXT,
+                        source VARCHAR(40)
+                    )`;
+                    db.query(sql, (err, result) => {});
+                    console.log(post.username + "Table Created!");
+                });        
+                response.redirect(`/login`);
+            });
+        }
     });
 });
 
