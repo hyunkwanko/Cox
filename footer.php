7e65a3579edbc29ce8a5391e134a61770b0e15a4
hyunkwanko <cjdshrwkd11@gmail.com>
2019-05-13 22:48:11 +0900

    COX

---
+++ footer.php
@@ -0,0 +1,76 @@
+   <footer class="footer_part">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-6 col-lg-3">
+               <div class="single_footer_part">
+                  <h4>About Us</h4>
+                  <p>Heaven fruitful doesn't over lesser days appear creeping seasons so behold bearing days open</p>
+                  <a href="index.html" class="footer_logo"> </a>
+               </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+               <div class="single_footer_part">
+                  <h4>Contact Info</h4>
+                  <p>Address :Your address goes
+                     here, your demo address.</p>
+                  <p>Phone : +8880 44338899</p>
+                  <p>Email : info@colorlib.com</p>
+               </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+               <div class="single_footer_part">
+                  <h4>Important Link</h4>
+                  <ul class="list-unstyled">
+                     <li><a href="">WHMCS-bridge</a></li>
+                     <li><a href="">Search Domain</a></li>
+                     <li><a href="">My Account</a></li>
+                     <li><a href="">Shopping Cart</a></li>
+                     <li><a href="">Our Shop</a> Our Shop</li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+               <div class="single_footer_part">
+                  <h4>Newsletter</h4>
+                  <p>Heaven fruitful doesn't over lesser in days. Appear creeping seasons deve behold bearing days open
+                  </p>
+               </div>
+            </div>
+         </div>
+         <hr>
+         <div class="row">
+            <div class="col-sm-6 col-lg-6">
+               <div class="copyright_text">
+                  <P><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                     Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  </P>
+               </div>
+            </div>
+            <div class="col-sm-6 col-lg-6">
+               <div class="footer_icon">
+                  <ul class="list-unstyled">
+                     <li><a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                     <li><a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a></li>
+                     <li><a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a></li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+   </footer>
+   <!-- jquery plugins here-->
+   <!-- jquery -->
+   <script src="<?php ROOT?>/static/js/jquery-1.12.1.min.js"></script>
+   <!-- popper js -->
+   <script src="<?php ROOT?>/static/js/popper.min.js"></script>
+   <!-- bootstrap js -->
+   <script src="<?php ROOT?>/static/js/bootstrap.min.js"></script>
+   <!-- easing js -->
+   <script src="<?php ROOT?>/static/js/jquery.magnific-popup.js"></script>
+   <!-- particles js -->
+   <script src="<?php ROOT?>/static/js/owl.carousel.min.js"></script>
+   <!-- easing js -->
+   <script src="<?php ROOT?>/static/js/jquery.easing.min.js"></script>
+   <!-- custom js -->
+   <script src="<?php ROOT?>/static/js/custom.js"></script>
